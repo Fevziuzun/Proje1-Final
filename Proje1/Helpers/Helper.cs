2deccee9b828c5f5a68e087b9597e4311a3f0064
mrxsh <mrxsh@DESKTOP-U41JNIS>
2023-06-16 05:21:42 +0300

    Proje dosyası ekle.

---
+++ Proje1/Helpers/Helper.cs
@@ -0,0 +1,72 @@
+﻿using Proje1.Enums;
+using Proje1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje1.Helpers
+{
+    class Helper
+    {
+        public static List<Movie> createMovies()
+        {
+            string basePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            return new List<Movie>()
+            {
+                new Movie()
+                {
+                    movieName = "Yıldızlar Arası",
+                    category = Category.bilimKurgu,
+                    minute = "2 Saat 49 Dakika",
+                    price = 50,
+                    picturePath = basePath + "/interstaller.jpeg"
+                },
+                   new Movie()
+                {
+                    movieName = "Inception",
+                    category = Category.macera,
+                    minute = "2 Saat 10 Dakika",
+                    price = 60,
+                    picturePath = basePath + "/inception.jpg"
+                },
+                new Movie()
+                {
+                    movieName = "Iron Man",
+                    category = Category.bilimKurgu,
+                    minute = "2 Saat 55 Dakika",
+                    price = 20,
+                    picturePath = basePath + "/ironman.jpg"
+                },
+                new Movie()
+                {
+                    movieName = "Lucy",
+                    category = Category.bilimKurgu,
+                    minute = "3 Saat 1 Dakika",
+                    price = 30,
+                    picturePath = basePath + "/lucy.jpg"
+                },
+                new Movie()
+                {
+                    movieName = "Yetenekli Bay Cage",
+                    category = Category.komedi,
+                    minute = "2 Saat 40 Dakika",
+                    price = 50,
+                    picturePath = basePath + "/massivetalentbycage.jpg"
+                },
+
+             new Movie()
+                {
+                    movieName = "The Last Of Us",
+                    category = Category.bilimKurgu,
+                    minute = "3 Saat 30 Dakika",
+                    price = 200,
+                    picturePath = basePath + "/thelastofus.jpg"
+                },
+
+
+            };
+        }
+    }
+}
---
+++ Proje1/Tickets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Proje1.Model;
+using Proje1.Nhibernate;
+
+namespace Proje1
+{
+    public partial class Tickets : Form
+    {
+        public Tickets()
+        {
+            InitializeComponent();
+        }
+
+        private void Tickets_Load(object sender, EventArgs e)
+        {
+            using (var session = NhibernateHelper.OpenSession()) 
+            {
+                var tickets = session.QueryOver<Sales>().List(); 
+
+                dataGridView1.DataSource = tickets; 
+
+
+            }
+        }
+    }
+}
